742b5baf360a7a9cea4cfa2cd81ed4077796f360
jean-pierre damen <jpdamen@cloudarchitect.me>
2020-02-06 13:44:15 +0100

    django.util.six > six

--- robots/models.py
+++ robots/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.six import u
+from six import u
 from django.utils.text import get_text_list
 from django.utils.translation import ugettext_lazy as _
 
