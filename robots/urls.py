24724329a82ab99cf5ce8a96a0a3256f38909405
besarbertasholli <besarbertasholli@hotmail.com>
2022-07-04 23:29:53 +0200

    Updated urls file to accept re_path method

--- robots/urls.py
+++ robots/urls.py
@@ -1,7 +1,12 @@
-from django.conf.urls import url
+import django
+
+if django.VERSION >= (3, 0):
+    from django.urls import re_path as url_path
+else:
+    from django.conf.urls import url as url_path
 
 from robots.views import rules_list
 
 urlpatterns = [
-    url(r'^$', rules_list, name='robots_rule_list'),
+    url_path(r'^$', rules_list, name='robots_rule_list'),
 ]
