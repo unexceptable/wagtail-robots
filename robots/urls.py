dbe4262183ce065bb500ce6b71732389e97ab199
Nick Moreton <nickmoreton@me.com>
2022-07-19 13:54:36 +0100

    Update urls.py

--- robots/urls.py
+++ robots/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from robots.views import rules_list
 
 urlpatterns = [
-    path('$', rules_list, name='robots_rule_list'),
+    path('', rules_list, name='robots_rule_list'),
 ]
